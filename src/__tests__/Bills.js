da83b0197ac29e7bac227a58368d0b52bbc990f1
Hammouti Ali <ali.hammouti@epsi.fr>
2022-07-07 16:45:41 +0200

    fix : implementation of integration test GET from mock data

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -7,8 +7,11 @@ import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store.js"
 
 import router from "../app/Router.js";
+jest.mock("../app/store", () => mockStore)
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -35,5 +38,88 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
     })
+  })
+})
+
+// test d'intégration GET
+describe("Given I am a user connected as an employee", () => {
+  describe("When I navigate to Bills page", () => {
+    test("fetches bills from mock API GET", async () => {
+      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      screen.getByText("Mes notes de frais");
+      screen.getByText("Nouvelle note de frais");
+
+      // Bill n°1
+      const billsTypeTransports = await waitFor(() => screen.getByText("Transports"))
+      expect(billsTypeTransports).toBeTruthy()
+      const contentTest1 = await waitFor(() => screen.getByText("test1"))
+      expect(contentTest1).toBeTruthy()
+
+      // Bill n°2
+      const billsTypeRestoAndBars = await waitFor(() => screen.getAllByText("Restaurants et bars"))
+      expect(billsTypeRestoAndBars).toBeTruthy()
+      const contentTest2 = await waitFor(() => screen.getAllByText("test2"))
+      expect(contentTest2).toBeTruthy()
+
+      // Bill n°3
+      const billsTypeOnlineServices = await waitFor(() => screen.getAllByText("Services en ligne"))
+      expect(billsTypeOnlineServices).toBeTruthy()
+      const contentTest3 = await waitFor(() => screen.getAllByText("test3"))
+      expect(contentTest3).toBeTruthy()
+    })
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills")
+        Object.defineProperty(
+          window,
+          'localStorage',
+          { value: localStorageMock }
+        )
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee',
+          email: "a@a"
+        }))
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.appendChild(root)
+        router()
+      })
+      test("fetches bills from an API and fails with 404 message error", async () => {
+
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 404"))
+            }
+          }
+        })
+        window.onNavigate(ROUTES_PATH.Dashboard)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/)
+        expect(message).toBeTruthy()
+      })
+
+      test("fetches messages from an API and fails with 500 message error", async () => {
+
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list: () => {
+              return Promise.reject(new Error("Erreur 500"))
+            }
+          }
+        })
+
+        window.onNavigate(ROUTES_PATH.Dashboard)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 500/)
+        expect(message).toBeTruthy()
+      })
+    })
+
   })
 })
